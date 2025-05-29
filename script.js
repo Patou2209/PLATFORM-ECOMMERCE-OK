e7e4bbcf2f51a44e7a633d5a919da92df4268465
kaziama <professionalgith@gmail.com>
2025-05-29 20:18:18 +0200

    updating nav and footer

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ if (close) {
 }
 
 
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+// Function to redirect to the home page
+document.querySelectorAll('.logoTexte').forEach(item => {
+    item.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
+});
+
+
+
